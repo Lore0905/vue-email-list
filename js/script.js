bdc2288d4ffd23fd70809ad31e5129bfc68fe77f
Lore0905 <lore.castelli4@gmail.com>
2021-12-15 18:12:06 +0100

    modifica

--- js/script.js
+++ js/script.js
@@ -5,26 +5,29 @@ var app5 = new Vue(
         el: '#root',
         data: {
             email: [],
-            visibleEmail: true,
+            visibleEmail: false,
             numberMail: 10,
             
         },
         methods:{
-            
+            visible: function(){
+                
+            }
             
         },
         created: function(){
             
-            for( let i = 0; this.email.length < this.numberMail; i++ ) {
+            for( let i = 0; i < this.numberMail; i++ ) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((singleEmail) => {
                     this.email.push(singleEmail.data.response)
                     
-                    console.log(this.email)
+                    
                 });
+                
             }
             
-            
+            console.log(this.email)
         }
     }
 );
